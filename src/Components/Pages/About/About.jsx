d8cb575f230997f1307756be2b374ec41c866c17
rkhetarpal <42208008+rkhetarpal@users.noreply.github.com>
2026-07-20 11:47:15 +0200

    Clean up commented code in About.jsx
    Removed commented-out certificate images from About page.

--- src/Components/Pages/About/About.jsx
+++ src/Components/Pages/About/About.jsx
@@ -245,8 +245,8 @@ const Home = () => {
 
               <div className={styles.slide}>
                 <img src={cert11} alt="Certificates" />
-                {/* <img src={cert12} alt="Certificates" />
-                <img src={cert13} alt="Certificates" />
+                <img src={cert12} alt="Certificates" />
+                {/* <img src={cert13} alt="Certificates" />
                 <img src={cert14} alt="Certificates" />
                 <img src={cert15} alt="Certificates" /> */}
                 <img src={cert4} alt="Certificates" />
